5c0dd0121d84777ee393b9f231ef4a8b3a166b29
Mateusz Paluszek <bok@servhost.pl>
2023-05-09 11:21:16 +0200

    Dodanie adnotacji w repozytoriach

--- src/Repository/CurrencyRateRepository.php
+++ src/Repository/CurrencyRateRepository.php
@@ -4,8 +4,17 @@ namespace App\Repository;
 
 use App\Entity\CurrencyRate;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\AbstractQuery;
 use Doctrine\Persistence\ManagerRegistry;
 
+/**
+ * @extends ServiceEntityRepository<CurrencyRate>
+ *
+ * @method CurrencyRate|null find($id, $lockMode = null, $lockVersion = null)
+ * @method CurrencyRate|null findOneBy(array $criteria, array $orderBy = null)
+ * @method CurrencyRate[]    findAll()
+ * @method CurrencyRate[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
 class CurrencyRateRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
