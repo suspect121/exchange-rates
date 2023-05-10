d5dfdb00ffcc3beaf6c99c644938fafada605b8e
Mateusz Paluszek <bok@servhost.pl>
2023-05-10 23:26:47 +0200

    Walidator danych dotyczących kursów walut z API NBP
    Walidator danych został opracowany z zastosowaniem klasy abstrakcyjnej Validate, która ułatwi w przyszłości wdrażanie kolejnych walidatorów.

--- src/Nbp/Validator/ExchangeRatesTableValidator.php
+++ src/Nbp/Validator/ExchangeRatesTableValidator.php
@@ -7,6 +7,7 @@
 namespace App\Nbp\Validator;
 
 use App\Nbp\ApiResponse;
+use App\Nbp\Exception\ValidateException;
 
 /**
  * Walidator odpowiedzi z API NBP dotyczącej kursów wymiany walut z wybranej tabeli
@@ -15,13 +16,59 @@ use App\Nbp\ApiResponse;
  * @copyright 2023 Mateusz Paluszek
  * @package App\Nbp
  */
-class ExchangeRatesTableValidator implements ValidatorInterface
+class ExchangeRatesTableValidator extends Validator implements ValidatorInterface
 {
     /**
      * @inheritDoc
      */
     public function validate(ApiResponse $api_response): void
     {
+        $response = $api_response->getResponseArray();
+        $this->checkMainData($response);
+        $this->checkRatesData($response);
+    }
 
+    /**
+     * Sprawdza podstawowe dane zwrócone przez API NBP
+     *
+     * Niniejsza metoda sprawdzana klucze będące najwyżej w hierarchii wraz z ich danymi.
+     *
+     * @param array $response
+     * @throws ValidateException
+     */
+    private function checkMainData(array $response): void
+    {
+        $allowed_keys = ['table', 'no', 'effectiveDate', 'rates'];
+        $required_keys = ['table', 'effectiveDate', 'rates'];
+        $expected_properties = [
+            'table' => ['type' => 'string', 'pattern' => '/^[A-C]$/'],
+            'no' => ['type' => 'string', 'pattern' => '/^[0-9A-Z\/]+\/NBP\/2[0-9]{3}$/'],
+            'effectiveDate' => ['type' => 'string', 'pattern' => '/^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/'],
+            'rates' => ['type' => 'array']
+        ];
+        $this->checkKeys($response, $allowed_keys, $required_keys);
+        $this->checkDataContent($response, $expected_properties);
+    }
+
+    /**
+     * Sprawdza dane zwrócone przez API NBP pod kluczem "rates"
+     *
+     * @param array $response
+     * @throws ValidateException
+     */
+    private function checkRatesData(array $response): void
+    {
+        foreach($response['rates'] as $data)
+        {
+            $allowed_keys = ['currency', 'code', 'mid'];
+            $required_keys = $allowed_keys;
+            $expected_properties = [
+                'currency' => ['type' => 'string', 'pattern' => '/^[a-zA-Z\p{L}() ]+$/u'],
+                'code' => ['type' => 'string', 'pattern' => '/^[A-Z]{3}$/'],
+                'mid' => ['type' => 'double']
+            ];
+            $this->checkKeys($data, $allowed_keys, $required_keys);
+            $this->checkDataContent($data, $expected_properties);
+        }
     }
 }
---
+++ src/Nbp/Validator/Validator.php
@@ -0,0 +1,116 @@
+<?php
+
+/*
+ * Copyright © 2023 Mateusz Paluszek
+ */
+
+namespace App\Nbp\Validator;
+
+use App\Nbp\Exception\ValidateException;
+
+/**
+ * Klasa abstrakcyjna dostarczająca metody pomocnicze dla walidatorów
+ *
+ * @author Mateusz Paluszek <bok@servhost.pl>
+ * @copyright 2023 Mateusz Paluszek
+ * @package App\Nbp\Validator
+ */
+abstract class Validator
+{
+    /**
+     * Sprawdza klucze danych pod względem zgodności z oczekiwaniami
+     *
+     * Sprawdzaniu podlega samo istnienie kluczy danych. Nie są weryfikowane ich wartości.
+     *
+     * @param array $data Dane których klucze będą sprawdzane
+     * @param array $allowed_keys Dozwolone klucze danych
+     * @param array $required_keys Wymagane klucze danych
+     * @throws ValidateException
+     */
+    protected function checkKeys(array $data, array $allowed_keys, array $required_keys = []): void
+    {
+        $data_keys = array_keys($data);
+        $this->checkDataHasAllowedKeys($data_keys, $allowed_keys);
+        if($required_keys !== [])
+        {
+            $this->checkDataHasRequiredKeys($data_keys, $required_keys);
+        }
+    }
+
+    /**
+     * Sprawdza wszystkie istniejące klucze danych pod względem tego czy są oczekiwane (niekoniecznie wymagane)
+     *
+     * @param array $data_keys Dostarczone klucze danych
+     * @param array $allowed_keys Dozwolone klucze danych
+     * @throws ValidateException Wyjątek zwracany, gdy któryś z dostarczonych kluczy nie jest uznawany, jako oczekiwany
+     */
+    private function checkDataHasAllowedKeys(array $data_keys, array $allowed_keys): void
+    {
+        $diff_allowed_keys = array_diff($data_keys, $allowed_keys);
+        if($diff_allowed_keys !== [])
+        {
+            if(count($diff_allowed_keys) > 1)
+            {
+                $message = 'Odpowiedź z API zawiera niedozwolone klucze: '.implode(' ', $diff_allowed_keys);
+            } else {
+                $message = 'Odpowiedź z API zawiera niedozwolony klucz "'.current($diff_allowed_keys).'"';
+            }
+            throw new ValidateException($message);
+        }
+    }
+
+    /**
+     * Sprawdza czy zostały dostarczone wszystkie wymagane klucze danych
+     *
+     * @param array $data_keys Dostarczone klucze danych
+     * @param array $required_keys Wymagane klucze danych
+     * @throws ValidateException Wyjątek zwracany, gdy nie wszystkie wymagane klucze zostały dostarczone
+     */
+    private function checkDataHasRequiredKeys(array $data_keys, array $required_keys): void
+    {
+        $diff_required_keys = array_diff($required_keys, $data_keys);
+        if($diff_required_keys !== [])
+        {
+            if(count($diff_required_keys) > 1)
+            {
+                $message = 'Odpowiedź z API nie zawiera wymaganych kluczy: '.implode(' ', $diff_required_keys);
+            } else {
+                $message = 'Odpowiedź z API nie zawiera wymaganego klucza "'.current($diff_required_keys).'"';
+            }
+            throw new ValidateException($message);
+        }
+    }
+
+    /**
+     * Sprawdza dane pod względem ich typów i treści
+     *
+     * @param array $data Dane które mają być przedmiotem weryfikacji
+     * @param array $expected_properties Spodziewane właściwości danych. Powinny być one przekazane w formie tablicy
+     *                                   gdzie każdy z kluczy tablicy, odpowiada nazwą kluczowi znajdującemu się w
+     *                                   weryfikowanych danych. W ten sposób następuje powiązanie oczekiwanych
+     *                                   właściwości danych z danymi, które mają być przedmiotem weryfikacji.
+     *
+     *                                   Dozwolone spodziewane właściwości danych to:
+     *                                   <div><b>type</b> - Typ danych, np. integer czy string (pełne nazwy)</div>
+     *                                   <div><b>pattern</b> - Wzorzec danych (dotyczy jedynie typu string)
+     * @throws ValidateException
+     */
+    protected function checkDataContent(array $data, array $expected_properties): void
+    {
+        foreach($data as $key => $value)
+        {
+            $expected = $expected_properties[$key];
+            $value_type = gettype($value);
+            if($expected['type'] !== $value_type)
+            {
+                $message = 'Odpowiedź z API zawiera nieprawidłowy typ danych dla klucza "'.$key.'": '.$value_type;
+                throw new ValidateException($message);
+            }
+            if(isset($expected['pattern']) && $value_type === 'string' && !preg_match($expected['pattern'], $value))
+            {
+                $message = 'Odpowiedź z API zawiera nieprawidłowe dane dla klucza "'.$key.'": '.$value;
+                throw new ValidateException($message);
+            }
+        }
+    }
+}
